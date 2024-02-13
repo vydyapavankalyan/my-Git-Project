7dc1eda7f5a40c189c8b7bc4c6d9861ede4a8f7d
vydyapavankalyan <vydyapavan@gmail.com>
2024-02-13 10:42:22 +0530

    Tochange

--- myfirst.java
+++ myfirst.java
@@ -3,6 +3,8 @@ public class test1
    public static void main(String []args)
    {
         System.out.println("Hello pavan");
+        System.out.println("Hi kalyan");
+        System.out.println("pavankalyan");
 
     }
 }
